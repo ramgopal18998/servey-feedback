ebf5b934c6a2f8a1c50b20f9719a1b1490e3f7de
ramgopal18998 <varmaramgopal.k@gmail.com>
2018-07-09 14:26:12 +0530

    oauth done

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+
+
+module.exports = {
+  googleClientID: '94909650814-aqh5irqt6keu2egehcaobi5jvhimlv5c.apps.googleusercontent.com',
+  googleClientSecret:'DQ46hpwontVEmX-XvA3AGbQt',
+  mongoDBUri:'mongodb://ramgopal18998:password123@ds111204.mlab.com:11204/node-oauth-db',
+  cookieKey: 'fkshdfasgfkasgdiufgiasdgfiuagdsiufgi'
+};
